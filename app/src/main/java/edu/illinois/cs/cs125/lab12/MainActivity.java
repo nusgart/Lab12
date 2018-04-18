bea0259c2239577a9a969700a76fe05d616a70cf
Nicholas Nusgart <nicknusgart@gmail.com>
2018-04-18 12:17:14 -0500

    Implemented lab

--- app/src/main/java/edu/illinois/cs/cs125/lab12/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/lab12/MainActivity.java
@@ -3,6 +3,10 @@ package edu.illinois.cs.cs125.lab12;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -18,11 +22,17 @@ import org.json.JSONObject;
  * Main class for our UI design lab.
  */
 public final class MainActivity extends AppCompatActivity {
-    /** Default logging tag for messages from the main activity. */
+    /**
+     * Default logging tag for messages from the main activity.
+     */
     private static final String TAG = "Lab12:Main";
 
-    /** Request queue for our API requests. */
+    /**
+     * Request queue for our API requests.
+     */
     private static RequestQueue requestQueue;
+
+    TextView jsonView, descView;
 
     /**
      * Run when this activity comes to the foreground.
@@ -37,7 +47,15 @@ public final class MainActivity extends AppCompatActivity {
         requestQueue = Volley.newRequestQueue(this);
 
         setContentView(R.layout.activity_main);
-
+        jsonView = findViewById(R.id.textViewJSON);
+        descView = findViewById(R.id.descriptionView);
+        final Button refresh = findViewById(R.id.refreshButton);
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.startAPICall();
+            }
+        });
         startAPICall();
     }
 
@@ -64,14 +82,34 @@ public final class MainActivity extends AppCompatActivity {
                         public void onResponse(final JSONObject response) {
                             try {
                                 Log.d(TAG, response.toString(2));
-                            } catch (JSONException ignored) { }
+                                jsonView.setText(response.toString(2));
+                                jsonView.setEnabled(true);
+
+                                String wd = response.getJSONArray("weather").getJSONObject(0).getString("description");
+                                double temp = response.getJSONObject("main").getDouble("temp"); // K
+                                // this is the average kinetic energy of each molecule
+                                double tempEv = temp * 8.6173303e-5 * 1000;
+                                double pressure = response.getJSONObject("main").getInt("pressure") / 10.0; //hPa-->kPa
+                                // to convert to atm, would be pressure /= 101.325; //
+                                int humidity = response.getJSONObject("main").getInt("humidity"); //percent
+                                // wind
+                                double windSpeed = response.getJSONObject("wind").getDouble("speed");
+                                double windDir = response.getJSONObject("wind").getDouble("deg");
+                                String description = String.format("Description: %s\nTemperature: %f Kelvin" +
+                                        "\nTemperature: %f milli-eV\nPressure: %f kPa\nPercent Humidity %d%%" +
+                                        "\nWind Speed: %f\nWind Direction: %f degrees", wd, temp,
+                                        tempEv, pressure, humidity, windSpeed, windDir);
+                                descView.setText(description);
+                            } catch (Throwable ignored) {
+                                Log.e(TAG, "Execption: ", ignored);
+                            }
                         }
                     }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(final VolleyError error) {
-                            Log.e(TAG, error.toString());
-                        }
-                    });
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.e(TAG, error.toString());
+                }
+            });
             requestQueue.add(jsonObjectRequest);
         } catch (Exception e) {
             e.printStackTrace();
